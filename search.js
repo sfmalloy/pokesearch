03748b3deb01e2bda46fa5e910e33180ecf060e0
Sean Malloy <techfan321@gmail.com>
2021-10-26 21:02:36 -0400

    Using browser local storage for caching

--- search.js
+++ search.js
@@ -1,29 +1,27 @@
-var cache = {};
-
 $(document).ready(() => {
     $("#doSearch").submit((e) => {
         var name = $("#doSearch").serializeArray()[0].value;
-
+        var cacheName = `pokesearch-${name}`
         e.preventDefault();
 
         $("#result").html("<p>Searching...</p>");
 
-        if (!(name in cache)) {
+        if (!(cacheName in localStorage)) {
             $.ajax({
                 type: "GET",
-                url: "https://pokeapi.co/api/v2/pokemon/" + name,
+                url: `https://pokeapi.co/api/v2/pokemon/${name}`,
                 success: (response) => {
                     console.log("GET call Success");
-                    cache[name] = response["sprites"]["other"]["official-artwork"]["front_default"]
-                    $("#result").html(`<img src="${cache[name]}">`);
+                    localStorage[cacheName] = response["sprites"]["other"]["official-artwork"]["front_default"]
+                    $("#result").html(`<img src="${localStorage[cacheName]}">`);
                 },
                 error: (response) => {
-                    $("#result").html(`<p>Pokémon "${name}" does not exist`);
+                    $("#result").html(`<p>Pokémon "pokesearch-${name}" does not exist</p>`);
                 }
             });
         } else {
             console.log("Image source in cache");
-            $("#result").html(`<img src="${cache[name]}">`);
+            $("#result").html(`<img src="${localStorage[cacheName]}">`);
         }
     });
 });
